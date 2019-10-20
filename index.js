f9c2aac9f32545b5518e1565fa4f6de38720b8ce
stalfos <stalfos1211@gmail.com>
2019-10-20 11:43:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 1337
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/slack', (req, res) => {
+  console.log('req:', req.body)
+  res.send('this is working')
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
