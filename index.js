72e00f06efc290da4fdac00575128d40013ef80c
stalfos <stalfos1211@gmail.com>
2019-10-20 12:33:39 -0500

    Blame Sandi

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const createMessage = info => {
         text: {
           type: 'mrkdwn',
           text:
-            'Here are some places you might want to check out.\n\n *Please select a restaurant:*'
+            "Here are some places you might want to check out.\n\n *Blame Sandi if this does't work:*"
         }
       },
       {
