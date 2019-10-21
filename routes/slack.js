0badd43d9499bb9c77771de4d252ac0b65482220
stalfos <stalfos1211@gmail.com>
2019-10-20 19:18:40 -0500

    Test show multiple

--- routes/slack.js
+++ routes/slack.js
@@ -6,8 +6,11 @@ module.exports = () => {
   router.post('/places-to-eat', async (req, res) => {
     console.log('req:', req.body)
     const text = req.body.text
+    console.log('text:', text)
 
-    let placesToEat = await utils.getPlacesToEat()
+    numberToShow = Number(text.match(/show\s(\d{2})/)[1])
+
+    let placesToEat = await utils.getPlacesToEat(numberToShow)
     let message = utils.createMessage(placesToEat)
 
     res.json(message)
